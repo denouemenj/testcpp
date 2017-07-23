07b9bf4e0b73175aa6608e7099f365d3a777cbfe
jiayuylinfs <jiayu.li.nfs@gmail.com>
2017-07-23 15:52:13 -0300

    Initial Commit

---
+++ testcpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testcpp
+//
+//  Created by Jiayu li on 2017-07-23.
+//  Copyright © 2017 Jiayu li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
